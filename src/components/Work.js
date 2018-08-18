71a196e138baff11a1f0d7a55f8afdd1acdfabca
Ro Diwa <rodiwaa@gmail.com>
2018-08-18 23:52:07 +0530

    updated work section

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,50 +7,61 @@ import GridListTileBar from '@material-ui/core/GridListTileBar'
 
 import bno from '../images/clients/bno.png'
 import ck12 from '../images/clients/ck12.png'
-import elm from '../images/clients/elm.png'
+// import elm from '../images/clients/elm.png'
 import med from '../images/clients/med.png'
 import msft from '../images/clients/msft.svg'
 import nt from '../images/clients/nt.png'
 import qnst from '../images/clients/qnst.png'
 
+import elmScreen from '../images/screens/elm-screen.png'
+// https://www.elliemae.com/solutions/consumer-engagement/encompass-consumer-connect
+import qnstScreen from '../images/screens/qnst-screen.png'
+import ck12Screen from '../images/screens/ck12-screen.png'
+import ntScreen from '../images/screens/nt-screen.png'
+import ntCompScreen from '../images/screens/nt-comp-screen.png'
+import medScreen from '../images/screens/meds-screen.png'
+import bnoScreen from '../images/screens/bno-screen.png'
+import msftScreen from '../images/screens/msft-screen.png'
+
+
 const tileData = [
   {
-    img: `${elm}`,
+    img: `${elmScreen}`,
     title: 'Consumer Connect',
     author: 'EllieMae'
   },
   {
-    img: `${qnst}`,
+    img: `${qnstScreen}`,
     title: 'Media Platform',
     author: 'Quinstreet'
   },
   {
-    img: `${ck12}`,
+    img: `${ck12Screen}`,
     title: 'Online Learning Portal',
     author: 'CK 12'
   },
   {
-    img: `${nt}`,
-    title: 'NT Companion (Mobile)',
-    author: 'NoodleTools'
-  },
-  {
-    img: `${nt}`,
+    img: `${ntScreen}`,
     title: 'NT Web App',
     author: 'NoodleTools'
   },
   {
-    img: `${med}`,
+    img: `${ntCompScreen}`,
+    title: 'NT Companion (Mobile)',
+    author: 'NoodleTools'
+  },
+  {
+    img: `${medScreen}`,
     title: 'HealthCare',
     author: 'Medsolis'
   },
   {
-    img: `${bno}`,
+    img: `${bnoScreen}`,
     title: 'Remote Airplay App',
     author: 'Bang & Olufsen'
   },
   {
-    img: `${msft}`,
+    img: `${msftScreen}`,
     title: 'SET Campaigns',
     author: 'Microsoft'
   }
@@ -71,20 +82,12 @@ export default class Skills extends React.PureComponent {
         </Grid>     
         <Grid item xs={12} sm={8} className='card-container' >
           <GridList cellHeight={180} className={classes.gridList} cols={3}>
-          {/* <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-            <ListSubheader component="div">December</ListSubheader>
-          </GridListTile> */}
           {tileData.map(tile => (
             <GridListTile key={tile.img}>
               <img src={tile.img} alt={tile.title} />
               <GridListTileBar
                 title={tile.title}
                 subtitle={<span>{tile.author}</span>}
-                // actionIcon={
-                //   <IconButton className={classes.icon}>
-                //     <InfoIcon />
-                //   </IconButton>
-                // }
               />
             </GridListTile>
           ))}
