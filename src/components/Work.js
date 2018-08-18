9028f245412d594b34c2b7a56b2a23bf9a9cfe4e
Ro Diwa <rodiwaa@gmail.com>
2018-08-18 11:13:58 +0530

    adding work images

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,72 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Button from '@material-ui/core/Button'
+
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import GridListTileBar from '@material-ui/core/GridListTileBar'
+// import ListSubheader from '@material-ui/core/ListSubheader'
+
+// import food1 from '../images/sample/food1.jpg'
+// import food2 from '../images/sample/food2.jpg'
+// import food3 from '../images/sample/food3.jpg'
+// import food4 from '../images/sample/food4.jpg'
+
+import bno from '../images/clients/bno.png'
+import ck12 from '../images/clients/ck12.png'
+import elm from '../images/clients/elm.png'
+import med from '../images/clients/med.png'
+import msft from '../images/clients/msft.svg'
+import nt from '../images/clients/nt.png'
+import qnst from '../images/clients/qnst.png'
+
+const tileData = [
+  {
+    img: `${elm}`,
+    title: 'Consumer Connect',
+    author: 'EllieMae'
+  },
+  {
+    img: `${qnst}`,
+    title: 'Media Platform',
+    author: 'Quinstreet'
+  },
+  {
+    img: `${ck12}`,
+    title: 'Online Learning Portal',
+    author: 'CK 12'
+  },
+  {
+    img: `${nt}`,
+    title: 'NT Companion (Mobile)',
+    author: 'NoodleTools'
+  },
+  {
+    img: `${nt}`,
+    title: 'NT Web App',
+    author: 'NoodleTools'
+  },
+  {
+    img: `${med}`,
+    title: 'HealthCare',
+    author: 'Medsolis'
+  },
+  {
+    img: `${bno}`,
+    title: 'Remote Airplay App',
+    author: 'Bang & Olufsen'
+  },
+  {
+    img: `${msft}`,
+    title: 'SET Campaigns',
+    author: 'Microsoft'
+  }
+]
+
+const classes = {
+  gridList: {
+    
+  }
+}
 
 export default class Skills extends React.PureComponent {
   render() {
@@ -12,35 +75,26 @@ export default class Skills extends React.PureComponent {
         <Grid item xs={12} sm={8} className='section-title'>
           Work & Projects
         </Grid>     
-        <Grid item xs={12} className='card-container' >
-          <Card className='skills-card'>
-            <CardContent>
-              sdasd
-            </CardContent>
-            <CardActions>
-              <Button size="small" variant="contained" color="secondary">Learn More</Button>
-            </CardActions>
-          </Card>
-          <Card className='skills-card'>
-            <CardContent>
-              sdasd
-            </CardContent>
-          </Card>
-          <Card className='skills-card'>
-            <CardContent>
-              sdasd
-            </CardContent>
-          </Card>
-          <Card className='skills-card'>
-            <CardContent>
-              sdasd
-            </CardContent>
-          </Card>
-          <Card className='skills-card'>
-            <CardContent>
-              sdasd
-            </CardContent>
-          </Card>
+        <Grid item xs={12} sm={8} className='card-container' >
+          <GridList cellHeight={180} className={classes.gridList} cols={3}>
+          {/* <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+            <ListSubheader component="div">December</ListSubheader>
+          </GridListTile> */}
+          {tileData.map(tile => (
+            <GridListTile key={tile.img}>
+              <img src={tile.img} alt={tile.title} />
+              <GridListTileBar
+                title={tile.title}
+                subtitle={<span>{tile.author}</span>}
+                // actionIcon={
+                //   <IconButton className={classes.icon}>
+                //     <InfoIcon />
+                //   </IconButton>
+                // }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
         </Grid>    
       </Grid>
     )
